f45b56d3f7bd7dd177c3997cac23d53cc19db2c3
Nicolas Cattoni <nico.cattoni@hotmail.com>
2025-05-06 12:26:11 +0200

    commit components creation

---
+++ crm_restaurante_frontend/src/app/interfaces/menu.interface.ts
@@ -0,0 +1,12 @@
+export interface MenuInterface {
+
+   id: number;
+    name: string;
+    firsts: string;
+    seconds: string; 
+    desserts: string;
+    price: number;
+    fecha_creacion: Date;
+    capacidad: number;
+}
+
---
+++ crm_restaurante_frontend/src/app/pages/menus/lista-menu/lista-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-menu',
+  imports: [],
+  templateUrl: './lista-menu.component.html',
+  styleUrl: './lista-menu.component.css'
+})
+export class ListaMenuComponent {
+
+}
+
+
