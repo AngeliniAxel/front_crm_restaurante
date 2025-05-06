79bae6449283d2c50768b1374bdf10abde094bf7
AngeliniAxel <axel.angelini@gmail.com>
2025-05-06 13:48:58 +0200

    Agregar rutas y httpClient

--- crm_restaurante_frontend/src/app/app.routes.ts
+++ crm_restaurante_frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/users/register/register.component';
+import { LoginComponent } from './pages/users/Login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
