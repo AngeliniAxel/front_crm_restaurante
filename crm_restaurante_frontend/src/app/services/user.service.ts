d8156f161670c3f83ac4c975b13e20efcc634bd7
AngeliniAxel <axel.angelini@gmail.com>
2025-05-06 13:48:22 +0200

    creacion servicio e interface user

---
+++ crm_restaurante_frontend/src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { lastValueFrom } from 'rxjs';
+
+type LoginBody = {
+  email: string;
+  password: string;
+};
+
+type LoginResponse = {
+  message: string;
+  token: string;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private baseUrl: string = 'http://localhost:3000/api/users';
+  private httpClient = inject(HttpClient);
+
+  register(body: User) {
+    return lastValueFrom(
+      this.httpClient.post<User>(`${this.baseUrl}/register`, body)
+    );
+  }
+
+  login(body: LoginBody) {
+    return lastValueFrom(
+      this.httpClient.post<LoginResponse>(`${this.baseUrl}/login`, body)
+    );
+  }
+
+  isLogged() {
+    if (localStorage.getItem('restaurant_token')) return true;
+
+    return false;
+  }
+}
