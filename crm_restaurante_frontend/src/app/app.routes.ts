3f7bd3f00135901279f2457c713f4f33b38ebbbd
AngeliniAxel <axel.angelini@gmail.com>
2025-05-07 10:40:53 +0200

    add auth and admin guard

--- crm_restaurante_frontend/src/app/app.routes.ts
+++ crm_restaurante_frontend/src/app/app.routes.ts
@@ -5,11 +5,16 @@ import { RegisterComponent } from './pages/users/register/register.component';
 import { LoginComponent } from './pages/users/Login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { EditMenuComponent } from './pages/menus/edit-menu/edit-menu.component';
+import { adminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'menu', component: ListaMenuComponent },
-  { path: 'menu/edit', component: EditMenuComponent },
+  {
+    path: 'menu/edit',
+    component: EditMenuComponent,
+    canActivate: [adminGuard],
+  },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
 ];
---
+++ crm_restaurante_frontend/src/app/guards/auth.guard.ts
@@ -0,0 +1,15 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+
+  const token = localStorage.getItem('restaurant_token');
+  if (!token) {
+    alert('No puedes acceder');
+    router.navigateByUrl('/login');
+    return false;
+  }
+
+  return true;
+};
