cdb5e263266cd8562c7855903c3dda6624042bf7
AngeliniAxel <axel.angelini@gmail.com>
2025-05-08 20:29:38 +0200

    fix user and register layout, and add validators

--- crm_restaurante_frontend/src/app/pages/users/Login/login.component.ts
+++ crm_restaurante_frontend/src/app/pages/users/Login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,16 +19,32 @@ export class LoginComponent {
   router = inject(Router);
 
   loginForm: FormGroup = new FormGroup({
-    email: new FormControl(),
-    password: new FormControl(),
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/), // Al menos una minúscula, una mayúscula y un número
+    ]),
   });
+
+  checkError(field: string, validator: string): boolean | undefined {
+    return (
+      this.loginForm.get(field)?.hasError(validator) &&
+      this.loginForm.get(field)?.touched
+    );
+  }
 
   async onSubmit() {
     try {
       const response = await this.userService.login(this.loginForm.value);
       localStorage.setItem('restaurant_token', response.token);
-    } catch (error) {
-      alert(error);
+      this.router.navigateByUrl('/');
+    } catch (error: any) {
+      alert(error.error.message);
     }
   }
 }
--- crm_restaurante_frontend/src/app/pages/users/register/register.component.ts
+++ crm_restaurante_frontend/src/app/pages/users/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { UserService } from '../../../services/user.service';
 import { Router } from '@angular/router';
 
@@ -14,19 +19,37 @@ export class RegisterComponent {
   router = inject(Router);
 
   registerForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(30),
+    ]),
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(20),
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/), // Al menos una minúscula, una mayúscula y un número
+    ]),
   });
+
+  checkError(field: string, validator: string): boolean | undefined {
+    return (
+      this.registerForm.get(field)?.hasError(validator) &&
+      this.registerForm.get(field)?.touched
+    );
+  }
 
   async onSubmit() {
     try {
       const newUser = await this.userService.register(this.registerForm.value);
       alert('registro correcto');
-      console.log(newUser);
       this.router.navigateByUrl('/login');
-    } catch (error) {
-      alert(error);
+    } catch (error: any) {
+      alert(error.error.message);
     }
   }
 }
